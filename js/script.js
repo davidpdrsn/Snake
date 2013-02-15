ed04eb3f46c09de7467ef11383af9d25e97e20a6
David Pedersen <david.pdrsn@gmail.com>
2013-02-15 13:37:01 +0100

    pause function

--- js/script.js
+++ js/script.js
@@ -202,4 +202,19 @@ $(function(){
     game.reset();
   });
 
+  var paused = false;
+  $('.pause').click(function(){
+    if (!paused) {
+      $(this).html('Unpause');
+      for (var i = 0; i < 9999; i += 1) {
+        window.clearInterval(i);
+      }
+      paused = true;
+    } else {
+      $(this).html('Pause');
+      game.move();
+      paused = false;
+    }
+  });
+
 });
